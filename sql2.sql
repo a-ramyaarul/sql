3e98f57b7fa6278db593b58eb2bcca61bd5b54f1
Ramya A <168287039+a-ramyaarul@users.noreply.github.com>
2024-04-28 17:34:15 +0530

    Add files via upload

---
+++ sql2.sql
@@ -0,0 +1,5 @@
+insert into teacher values("joe",18,"nece",1h)
+insert into teacher values("e",8,"ecne",18)
+insert into teacher values("je",1,"ecee",14)
+                                                                        
+select age from teacher 
